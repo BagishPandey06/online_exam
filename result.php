888451683b77c9d7d2d6077efad4dd3abc6cdc98
Bagishpandey <bagishpandey999@gmail.com>
2020-10-31 19:44:26 +0530

    done

--- result.php
+++ result.php
@@ -10,24 +10,22 @@
  */
 require 'admin/config.php';
 require 'header.php';
-global $id,$your_score;
+global $id;
+$mr=1;
+$yousecured=0;
 $id=$_REQUEST['id'];
 if (empty($_SESSION['userdata'])) {
     echo "<script>alert('login first')</script>";
     header('location:index.php');
 }
 $session=$_SESSION['sessionid'];
-$sql="select * from answer where `test_id`=$id && `sessionid`=$session";
-$result=$con->query($sql);
-$count=num_rows($result);
-if ($count>0) {
-    while ($rows=$result->fetch_assoc()) {
-       
-        if ($rows['userans']==$rows['crtans']) {
-            $your_score=$your_score+$mr;
-        } 
-    }
+$a="SELECT * from answer where `test_id`=$id && `sessionid`=$session";
+$result=mysqli_query($con, $a);
+while ($rows=mysqli_fetch_array($result)) {
+    if ($rows['userans']==$rows['crtans']) {
+            $yousecured=$yousecured+$mr;
+    }    
 }
-echo '<p>Your score out of 10 is'.$your_score.'</p>';
+echo '<p>Your score out of 10 is'.$yousecured.'</p>';
 ?>
 <?php require 'footer.php';?>
